457608af4fef6e02eba883dbda471ebd14ab4c05
omontiel5-lgtm <omontiel5@gmail.com>
2026-05-07 16:06:40 -0400

    Add files via upload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,6 +23,7 @@ export default function HomePage() {
         <Guarantees />
         <Authority />
         <Speakers />
+        <FinalCTADual />
         <Advisors />
         <Process />
         <FAQ />
